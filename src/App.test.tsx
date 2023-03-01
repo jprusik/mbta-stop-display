4546630f8b4eb09dbe91ba9ebe180b654f850ad3
Jon Prusik <jprusik@classynemesis.com>
2023-03-01 16:22:37 -0500

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { App } from './App';
+import {I18nextProvider} from 'react-i18next';
+import {render, screen} from '@testing-library/react';
+import {App} from './App';
+import i18n from './i18n';
 
 test('renders the App', () => {
-  render(<App />);
-  const loadingState = screen.getByText(/Data is loading../i);
+  render(<I18nextProvider i18n={i18n}><App /></I18nextProvider>);
+  const loadingState = screen.getByText(/Please select a route./i);
   expect(loadingState).toBeInTheDocument();
 });
