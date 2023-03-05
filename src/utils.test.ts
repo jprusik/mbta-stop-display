d6f76cba97fce8198e82b2eea1f9a96d33c8ce54
Jon Prusik <jprusik@classynemesis.com>
2023-03-05 13:04:40 -0500

    add test

---
+++ src/utils.test.ts
@@ -0,0 +1,20 @@
+/**
+ * @jest-environment node
+ */
+import {parseBusNumberStringForSort} from 'utils';
+
+const testBusRouteNames = [
+  {given: '62/76', expected: 5450},
+  {given: '34E', expected: 5152},
+  {given: 'SL1', expected: 837649},
+  {given: 'CT2', expected: 678450},
+  {given: '71', expected: 5549}
+];
+
+test('parses bus route names for sorting, correctly', () => {
+  testBusRouteNames.forEach(({given, expected}) => {
+    const parsedValue = parseBusNumberStringForSort(given)
+
+    expect(parsedValue).toEqual(expected);
+  });
+});
